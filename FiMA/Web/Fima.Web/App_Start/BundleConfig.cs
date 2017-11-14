cbd499ca793fbce8f9d3fddb23b66c07b6539ce3
Zahari Dimitrov <zach_dimitrov@yahoo.com>
2017-11-14 21:59:26 +0200

    changes in styles added font awesome

--- FiMA/Web/Fima.Web/App_Start/BundleConfig.cs
+++ FiMA/Web/Fima.Web/App_Start/BundleConfig.cs
@@ -15,12 +15,16 @@
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include("~/Scripts/jquery-{version}.js"));
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include("~/Scripts/jquery.validate*"));
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include("~/Scripts/bootstrap.js"));
+
+            // bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include("~/Scripts/bootstrap.js"));
         }
 
         private static void RegisterStyles(BundleCollection bundles)
         {
-            bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/bootstrap.css", "~/Content/site.css"));
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                "~/Content/bootstrap.css",
+                "~/Content/site.css",
+                "~/Content/font-awesome.css"));
         }
     }
 }
