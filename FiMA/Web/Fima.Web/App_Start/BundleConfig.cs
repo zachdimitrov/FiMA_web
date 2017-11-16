e4d22e0c87b81efd7f86d2c84b316320ef626056
zachdimitrov <zahari_d@abv.bg>
2017-11-16 08:48:33 +0200

    created sidebar

--- FiMA/Web/Fima.Web/App_Start/BundleConfig.cs
+++ FiMA/Web/Fima.Web/App_Start/BundleConfig.cs
@@ -15,8 +15,7 @@
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include("~/Scripts/jquery-{version}.js"));
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include("~/Scripts/jquery.validate*"));
-
-            // bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include("~/Scripts/bootstrap.js"));
+            bundles.Add(new ScriptBundle("~/bundles/local").Include("~/Scripts/sidebar.js"));
         }
 
         private static void RegisterStyles(BundleCollection bundles)
--- FiMA/Web/Fima.Web/Scripts/_references.js
+++ FiMA/Web/Fima.Web/Scripts/_references.js
@@ -3,3 +3,4 @@
 /// <reference path="jquery.validate.unobtrusive.js" />
 /// <reference path="jquery-2.2.0.js" />
 /// <reference path="jquery-2.2.0.min.js" />
+/// <reference path="sidebar.js" />
