b585532feca88acb19bf0d902ca7b38863b1e12b
zachdimitrov <zahari_d@abv.bg>
2017-11-22 00:06:43 +0200

    added missing files in reverting

---
+++ FiMA/Web/Fima.Web/App_Start/DatabaseConfig.cs
@@ -0,0 +1,14 @@
+﻿namespace Fima.Web
+{
+    using System.Data.Entity;
+    using Data;
+    using Data.Migrations;
+
+    public static class DatabaseConfig
+    {
+        public static void Config()
+        {
+            // Database.SetInitializer(new MigrateDatabaseToLatestVersion<ApplicationDbContext, Configuration>());
+        }
+    }
+}
