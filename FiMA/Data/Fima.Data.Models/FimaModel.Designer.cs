2f7617bf2b9af34566c49e25b08320e19bfe97f7
zachdimitrov <zahari_d@abv.bg>
2017-11-22 09:23:42 +0200

    finish merge identity to db

--- FiMA/Data/Fima.Data.Models/FimaModel.Designer.cs
+++ FiMA/Data/Fima.Data.Models/FimaModel.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\Zach\Documents\Dev\FiMA_web\FiMA\Data\Fima.Data.Models\FimaModel.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\Zach\Documents\MVC5 Project\FiMA_web\FiMA\Data\Fima.Data.Models\FimaModel.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
--- FiMA/Web/Fima.Web/Startup.cs
+++ FiMA/Web/Fima.Web/Startup.cs
@@ -12,34 +12,28 @@ namespace Fima.Web
 {
     public partial class Startup
     {
-        private ApplicationDbContext context;
-
-        public Startup(ApplicationDbContext context)
-        {
-            this.context = context;
-        }
-
         public void Configuration(IAppBuilder app)
         {
             this.ConfigureAuth(app);
-            this.SeedData(this.context);
+            SeedData();
         }
 
-        private void SeedData(ApplicationDbContext context)
+        private static void SeedData()
         {
-            const string AdministratorUsername = "admin";
-            const string AdministratorEmail = "admin@fima.com";
-            const string AdministratorPassword = "123qwe";
+            var context = new ApplicationDbContext();
+            const string administratorUsername = "admin";
+            const string administratorEmail = "admin@fima.com";
+            const string administratorPassword = "123qwe";
 
-            this.SeedAdmin(
+            SeedAdmin(
             context,
-            AdministratorUsername,
-            AdministratorEmail,
-            AdministratorPassword);
+            administratorUsername,
+            administratorEmail,
+            administratorPassword);
         }
 
-        private void SeedAdmin(
-            ApplicationDbContext context,
+        private static void SeedAdmin(
+            DbContext context,
             string administratorUsername,
             string administratorEmail,
             string administratorPassword)
@@ -48,39 +42,41 @@ namespace Fima.Web
             var roleStore = new Fima.Data.Models.FimaRoleStore(context);
             var roleManager = new RoleManager<FimaRole, int>(roleStore);
 
-            if (!roleManager.RoleExists("Administrator"))
+            if (roleManager.RoleExists("Administrator"))
             {
-                var adminRole = new FimaRole { Name = "Administrator" };
-                var subscriberRole = new FimaRole { Name = "Subscriber" };
-                var frontOfficeRole = new FimaRole { Name = "FrontOffice" };
-                var backOfficeRole = new FimaRole { Name = "BackOffice" };
-                var accauntingRole = new FimaRole { Name = "Accountant" };
-                var walletManagerRole = new FimaRole { Name = "WalletManager" };
-                var riskManagerRole = new FimaRole { Name = "RiskManager" };
-
-                roleManager.Create(adminRole);
-                roleManager.Create(subscriberRole);
-                roleManager.Create(frontOfficeRole);
-                roleManager.Create(backOfficeRole);
-                roleManager.Create(accauntingRole);
-                roleManager.Create(walletManagerRole);
-                roleManager.Create(riskManagerRole);
-
-                var userStore = new Fima.Data.Models.FimaUserStore(context);
-                var userManager = new UserManager<FimaUser, int>(userStore);
-
-                var user = new FimaUser
-                {
-                    UserName = administratorUsername,
-                    Email = administratorEmail,
-                    EmailConfirmed = true
-                };
-
-                userManager.Create(user, administratorPassword);
-                userManager.AddToRole(user.Id, adminRole.Name);
-
-                context.SaveChanges();
+                return;
             }
+
+            var adminRole = new FimaRole { Name = "Administrator" };
+            var subscriberRole = new FimaRole { Name = "Subscriber" };
+            var frontOfficeRole = new FimaRole { Name = "FrontOffice" };
+            var backOfficeRole = new FimaRole { Name = "BackOffice" };
+            var accauntingRole = new FimaRole { Name = "Accountant" };
+            var walletManagerRole = new FimaRole { Name = "WalletManager" };
+            var riskManagerRole = new FimaRole { Name = "RiskManager" };
+
+            roleManager.Create(adminRole);
+            roleManager.Create(subscriberRole);
+            roleManager.Create(frontOfficeRole);
+            roleManager.Create(backOfficeRole);
+            roleManager.Create(accauntingRole);
+            roleManager.Create(walletManagerRole);
+            roleManager.Create(riskManagerRole);
+
+            var userStore = new Fima.Data.Models.FimaUserStore(context);
+            var userManager = new UserManager<FimaUser, int>(userStore);
+
+            var user = new FimaUser
+            {
+                UserName = administratorUsername,
+                Email = administratorEmail,
+                EmailConfirmed = true
+            };
+
+            userManager.Create(user, administratorPassword);
+            userManager.AddToRole(user.Id, adminRole.Name);
+
+            context.SaveChanges();
         }
     }
 }
