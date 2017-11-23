0811efca321ae54289f2ee6a79309bd39c759fe8
zachdimitrov <zach_dimitrov@yahoo.com>
2017-11-23 17:52:37 +0200

    added change users and roles

--- FiMA/Web/Fima.Web/Areas/Administration/Controllers/HomeController.cs
+++ FiMA/Web/Fima.Web/Areas/Administration/Controllers/HomeController.cs
@@ -7,13 +7,15 @@
     using Data.DbModels;
     using Infrastructure.Mapping;
     using Models;
+    using Web.Controllers;
 
-    public class HomeController : Controller
+    [Authorize(Roles = "Администратор")]
+    public class HomeController : BaseController
     {
         private readonly IFimaRepository<FimaUsers> users;
         private readonly IFimaRepository<FimaRoles> roles;
 
-        public HomeController(IFimaRepository<FimaUsers> users, IFimaRepository<FimaRoles> roles)
+        public HomeController(IFimaRepository<FimaUsers> users, IFimaRepository<FimaRoles> roles) 
         {
             this.users = users;
             this.roles = roles;
@@ -40,6 +42,56 @@
                 .ToList();
 
             return this.View(rolesModel);
+        }
+
+        [HttpGet]
+        public ActionResult UserEdit(int id)
+        {
+            var entity = this.users.GetById(id);
+            var model = this.Mapper.Map<FimaUsersViewModel>(entity);
+
+            return this.View(model);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult UserEdit(FimaUsersViewModel model)
+        {
+            if (!this.ModelState.IsValid)
+            {
+                return this.View(model);
+            }
+
+            var entity = this.users.GetById(model.Id);
+            this.Mapper.Map(model, entity);
+            this.users.Save();
+
+            return this.View(model);
+        }
+
+        [HttpGet]
+        public ActionResult RoleEdit(int id)
+        {
+            var entity = this.roles.GetById(id);
+            var model = this.Mapper.Map<FimaRolesViewModel>(entity);
+
+            return this.View(model);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult RoleEdit(FimaRolesViewModel model)
+        {
+            if (!this.ModelState.IsValid)
+            {
+                return this.View(model);
+            }
+
+            var entity = this.roles.GetById(model.Id);
+            this.Mapper.Map(model, entity);
+            this.roles.Save();
+
+            return this.View(model);
         }
     }
 }
