d75991cab7e4fbaa976ec2edf2e4ab20e9f47a7e
zachdimitrov <zahari_d@abv.bg>
2017-11-27 09:09:37 +0200

    added pyre-css and other libs

--- FiMA/Fima.Common/GlobalConstants.cs
+++ FiMA/Fima.Common/GlobalConstants.cs
@@ -10,5 +10,29 @@
         public const string WalletManagerRoleName = "управление на портфейл";
         public const string RiskManagerRoleName = "управление на риска";
         public const int PasswordMinLength = 1;
+
+        public static readonly string[] PerTypes = new[]
+        {
+            "българско физическо лице",
+            "българско юридическо лице",
+            "чуждестранно физическо лице",
+            "чуждестранно юридическо лице"
+        };
+
+        public static readonly string[] ContractType = new[]
+        {
+            "служител",
+            "пълномощник"
+        };
+
+        public static readonly string[] OrganisationType = new[]
+        {
+            "банка",
+            "ООД",
+            "ЕООД",
+            "АД",
+            "ЕТ",
+            "няма"
+        };
     }
 }
