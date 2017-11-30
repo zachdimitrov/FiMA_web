36b38652974be872ee28511d312335fb15a17ae8
Zahari Dimitrov <zach_dimitrov@yahoo.com>
2017-11-30 10:04:39 +0200

    fixed bug in IBAN and BIC

---
+++ FiMA/Web/Fima.Web/Areas/Administration/Controllers/CommonDataController.cs
@@ -0,0 +1,18 @@
+﻿namespace Fima.Web.Areas.Administration.Controllers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Web;
+    using System.Web.Mvc;
+    using Fima.Web.Controllers;
+
+    [Authorize(Roles = "администратор")]
+    public class CommonDataController : BaseController
+    {
+        // Countries
+        // Banks
+        // Towns
+        // Municipalities
+    }
+}
